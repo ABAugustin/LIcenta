5852a4ecebe400e2835c328b78cb4a661dde5181
ABAugustin <topciu.augustin21@gmail.com>
2024-08-19 20:30:29 +0200

    changes

--- LicentaServer/Packages/ConnectionHandler.py
+++ LicentaServer/Packages/ConnectionHandler.py
@@ -1,14 +1,14 @@
 import threading
 class ConnectionHandler:
     def __init__(self):
-        self.user_count_no = 1
+        self.user_ip_no = 1
         self.lock = threading.Lock()
 
     def toggle_user_count(self):
         with self.lock:
             # Ensure user_count_no toggles between 1 and 2
-            if self.user_count_no == 1:
-                self.user_count_no = 2
+            if self.user_ip_no == 1:
+                self.user_ip_no = 2
             else:
-                self.user_count_no = 1
-            return self.user_count_no
+                self.user_ip_no = 1
+            return self.user_ip_no
--- LicentaServer/Packages/GenerateCerts.py
+++ LicentaServer/Packages/GenerateCerts.py
@@ -1,11 +1,8 @@
 from Headers.headers import *
 
 
-def generate_greeting_certificate(cert_dir, user_id, user_count_no):
-    user_id += 1
+def generate_greeting_certificate(cert_dir, user_ip_no, user_count_no):
     user_count_no += 1
-    if user_id >= 3:
-        user_id = 0
 
     # Generate private key
     key = rsa.generate_private_key(
@@ -32,7 +29,7 @@ def generate_greeting_certificate(cert_dir, user_id, user_count_no):
     # Create custom extensions
     custom_extension_1 = x509.UnrecognizedExtension(oid_custom_e, bytes(e_rsa, 'utf-8'))
     custom_extension_2 = x509.UnrecognizedExtension(oid_custom_n, bytes(n_rsa, 'utf-8'))
-    custom_extension_3 = x509.UnrecognizedExtension(oid_custom_id, bytes(str(user_id), 'utf-8'))
+    custom_extension_3 = x509.UnrecognizedExtension(oid_custom_id, bytes(str(user_ip_no), 'utf-8'))
 
 
 
@@ -53,7 +50,7 @@ def generate_greeting_certificate(cert_dir, user_id, user_count_no):
 
     # Save the private key and certificate to files
 
-    cert_dir = check_and_create_folder(cert_dir, str(user_id))
+    cert_dir = check_and_create_folder(cert_dir, str(user_count_no))
 
     with open(os.path.join(cert_dir, "private_key_greetings_cert.pem"), "wb") as key_file:
         key_file.write(key.private_bytes(
@@ -67,7 +64,7 @@ def generate_greeting_certificate(cert_dir, user_id, user_count_no):
 
     print("Private key and certificate with integer extensions created successfully.")
 
-    return user_count_no, user_id
+    return user_count_no, user_ip_no
 
 
 def check_and_create_folder(cert_dir, folder_name):
