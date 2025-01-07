ca5fdec3fb08e5057c881baba8f2fe2d55eed164
ABAugustin <166854797+ABAugustin@users.noreply.github.com>
2025-01-07 15:57:59 +0200

    Back to origins op

--- LicentaServer/main.py
+++ LicentaServer/main.py
@@ -1,106 +1,118 @@
 import json
-import threading
-import os
-import socket
 from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.primitives import hashes
 from AES.AESOperations import derive_key, encrypt_message, decrypt_message
-from DTOs.DTOOperations import extract_wg_dto_data
+from DTOs.DTOOperations import receive_dto_data, decrypt_dto_data, extract_wg_dto_data, send_dto
 from DTOs.PairDTO import PairDTO
 from DiffieHellman.DH import dh_generate_private_key, dh_generate_public_key, compute_shared_secret
-from Headers.headers import server_prt
 from Packages.ConnectionHandler import ConnectionHandler
-from Packages.MongoMethods import insert_data_into_db, create_match_safe_words_db, remove_duplicate_pairs, \
-    drop_collection, get_pair_data
-from Packages.SandRCerts import generate_greeting_certificate, send_certificate, clear_buffer, receive_data, send_data
-import time
+from Packages.MongoMethods import insert_data_into_db, create_match_safe_words_db
+from Packages.SandRCerts import *
 
-# Create a global lock for database operations
 
 def handle_client(client_socket, cert_dir):
-    try:
-        user_ip_no = connection_handler.toggle_user_id_no()
-        user_count_no = connection_handler.toggle_user_count_no()
+    user_ip_no = connection_handler.toggle_user_id_no()
+    user_count_no = connection_handler.toggle_user_count_no()
 
-        # AICI SE GENEREAZA GREETING CERTIFICATE
-        private_key = generate_greeting_certificate(cert_dir, user_ip_no, user_count_no)
 
-        # AICI SE TRIMITE GREETING CERTIFICATE
-        send_certificate(client_socket, cert_dir, cert_file='/' + str(user_count_no) + "/greeting_certificate.pem")
-        print("~~~~~~~~~ A fost trimis certificatul ~~~~~~~~~~~~")
-        clear_buffer(client_socket)
+    # AICI SE GENEREAZA GREETING CERTIFICATE
 
-        # AICI INCEPE DIFFIE HELLMAN
-        server_private_key_dh = dh_generate_private_key()
-        server_public_key_dh = dh_generate_public_key(server_private_key_dh)
+    private_key = generate_greeting_certificate(cert_dir, user_ip_no, user_count_no)
 
-        encrypted_client_public_key_dh = receive_data(client_socket)
-        print("~~~~~~~~~ A fost primita cheia DH client ~~~~~~~~~~~~")
-        clear_buffer(client_socket)
+    #AICI SE TRIMITE GREETING CERTIFICATE
 
-        client_public_key_dh_bytes = private_key.decrypt(
-            encrypted_client_public_key_dh,
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None
-            )
+    send_certificate(client_socket, cert_dir, cert_file='/' + str(user_count_no) + "/greeting_certificate.pem")
+
+    print("~~~~~~~~~ A fost trimis certificatul ~~~~~~~~~~~~")
+
+    clear_buffer(client_socket)
+
+    # AICI INCEPE DIFFIE HELLMAN
+
+    # AICI GENERAM CHEILE PRIVATE SI PUBLICE ALE SERVERULUI PT DH
+    server_private_key_dh = dh_generate_private_key()
+    server_public_key_dh = dh_generate_public_key(server_private_key_dh)
+
+
+    # AICI PRIMIM CHEIA CRIPTATA A CLIENTULUI PT DH
+    encrypted_client_public_key_dh = receive_data(client_socket)
+
+    print("~~~~~~~~~ A fost primita cheia DH client ~~~~~~~~~~~~")
+    clear_buffer(client_socket)
+
+    # AICI DECRIPTAM CHEIA PUBLICA A CLIENTULUI PT DH CU CHEIA PRIVATA A SERVERULUI SSL
+    client_public_key_dh_bytes = private_key.decrypt(
+        encrypted_client_public_key_dh,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
         )
+    )
 
-        client_public_key_dh = int.from_bytes(client_public_key_dh_bytes, "big")
-        server_public_key_dh_bytes = server_public_key_dh.to_bytes((server_public_key_dh.bit_length() + 7) // 8, "big")
-        send_data(client_socket, server_public_key_dh_bytes)
-        clear_buffer(client_socket)
-        print("~~~~~~~~~ A fost trimis cheia serverului catre client DH ~~~~~~~~~~~~")
-
-        shared_secret = compute_shared_secret(client_public_key_dh, server_private_key_dh)
-
-        # AICI PRIMIM DTO-ul WIREGUARD
-        wg_dto_encrypted = receive_data(client_socket)
-        clear_buffer(client_socket)
-
-        aes_key = derive_key(shared_secret)
-        print("aes key")
-        print(aes_key)
-        wg_dto_decrypted = decrypt_message(aes_key, wg_dto_encrypted)
-        print("data to put in wg")
-        print(wg_dto_decrypted)
-
-        wg_dto = extract_wg_dto_data(wg_dto_decrypted)
-        safe_word, machine_ip, pub_key, sub_ip, port_ip, told_word = wg_dto.to_tuple()
-
-        # Thread-safe database operations
-
-        insert_data_into_db(safe_word, machine_ip, pub_key, sub_ip, port_ip, told_word)
-
-        create_match_safe_words_db()
-
-        #remove_duplicate_pairs()
+    # AICI TRANSFORMAM CHEIA PRIMITA DE LA CLIENT IN INTEGER
+    client_public_key_dh = int.from_bytes(client_public_key_dh_bytes, "big")
 
 
-        public_key, ip_address, port, endpoint = get_pair_data(pub_key, machine_ip, safe_word, port_ip, told_word)
+    # AICI TRIMITEM CHEIA PUBLICA A SERVERULUI DH CATRE CLIENT + BYTES
+    server_public_key_dh_bytes = server_public_key_dh.to_bytes((server_public_key_dh.bit_length() + 7) // 8, "big")
 
-        print("//////////////////////////////////////////")
-        print(public_key, ip_address, port, endpoint)
-        print("//////////////////////////////////////////")
+    send_data(client_socket,server_public_key_dh_bytes)
+    clear_buffer(client_socket)
 
-        pair_dto = PairDTO(public_key, ip_address, port, endpoint)
-        pair_dto_json = json.dumps(pair_dto.to_dict()).encode("utf-8")
-        dto_pair_encrypted = encrypt_message(aes_key, pair_dto_json)
+    print("~~~~~~~~~ A fost trimis cheia serverului catre client DH ~~~~~~~~~~~~")
 
-        send_data(client_socket, dto_pair_encrypted)
-        clear_buffer(client_socket)
 
-        # Drop collection safely
-        with db_lock:
-            drop_collection()
 
-    except Exception as e:
-        print(f"Error handling client: {e}")
-    finally:
-        print("Closing client socket")
-        client_socket.close()
+    # AICI CREAM SHARED SECRET AL DH CU CARE CRIPTAM MAI INCOLO CU AES
 
+    shared_secret = compute_shared_secret(client_public_key_dh, server_private_key_dh)
+
+    #AICI PRIMIM DTO-ul WIREGUARD
+    wg_dto_encrypted = receive_data(client_socket)
+    clear_buffer(client_socket)
+
+    # AICI DECRIPTAM DTO-ul WIREGUARD CU AES ( cu care teoretic e criptat)
+
+    aes_key = derive_key(shared_secret)
+
+    print("aes key")
+    print(aes_key)
+    wg_dto_decrypted = decrypt_message(aes_key, wg_dto_encrypted)
+
+    print("data to put in wg")
+    print(wg_dto_decrypted)
+    # AICI EXTRAGEM DATELE DIN JSON LE BAGAM IN DTO SI SEPARAT
+
+    wg_dto = extract_wg_dto_data(wg_dto_decrypted)
+    safe_word, machine_ip, pub_key, sub_ip, port_ip, told_word = wg_dto.to_tuple()
+
+    # AICI TREABA BAZA DE DATE NU MAI STIU
+
+    insert_data_into_db(safe_word, machine_ip, pub_key, sub_ip, port_ip, told_word)
+
+    create_match_safe_words_db()
+
+
+    # AICI SE IAU DATELE PAIR_ului
+    public_key, ip_address, port, endpoint = get_pair_data(pub_key, machine_ip, safe_word, port_ip, told_word)
+
+    # AICI SE CREAZA DTO ul de pair
+    pair_dto = PairDTO(public_key, ip_address, port, endpoint)
+
+    # wg_dto  -> json -> bytes
+    pair_dto_json = json.dumps(pair_dto.to_dict()).encode("utf-8")
+
+    # AICI SE CRIPTEAZA DTO ul PAIR CU AES
+
+    dto_pair_encrypted=encrypt_message(aes_key, pair_dto_json)
+
+    # AICI SE TRIMITE OBIECTUL DTO CRIPTAT CU AES
+    send_data(client_socket, dto_pair_encrypted)
+    clear_buffer(client_socket)
+
+    client_socket.close()
+
+    #207.180.196.203
 def start_server(cert_dir, host='0.0.0.0', port=server_prt):
     if not os.path.exists(cert_dir):
         os.makedirs(cert_dir)
@@ -115,10 +127,11 @@ def start_server(cert_dir, host='0.0.0.0', port=server_prt):
         client_socket, client_address = server_socket.accept()
         print(f"Connection from {client_address}")
 
-        # Generate greeting certificate
+        # generate greeting certificate
         thread = threading.Thread(target=handle_client, args=(client_socket, cert_dir))
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
 
 if __name__ == '__main__':
     connection_handler = ConnectionHandler()
